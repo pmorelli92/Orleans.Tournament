21a32ded0a4fbe90c8b4c3c120fede82f776e35c
Pablo Morelli <pabmorelli@gmail.com>
2019-10-07 13:33:19 +0200

    removed domain

--- src/Domain/Constants.cs
+++
@@ -1,9 +0,0 @@
-namespace Orleans.Tournament.Domain
-{
-    public class Constants
-    {
-        public const string StreamNamespace = "Application";
-        public const string TeamStream = "Team-Stream";
-        public const string TournamentStream = "Tournament-Stream";
-    }
-}
